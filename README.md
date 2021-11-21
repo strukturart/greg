9575c45e465b3b759c34606a896c1d0680bc11d7
strukturart <print@strukturart.com>
2021-11-21 12:08:35 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,45 @@
+<img src="/images/logo.svg" width="228"/>
+
+# Greg
+
+![badge-release](https://img.shields.io/github/v/release/strukturart/feedolin?include_prereleases&style=plastic)
+[![badge-bhackers](https://img.shields.io/badge/bHackers-bHackerStore-orange)](https://store.bananahackers.net/#feedolin)
+![badge-downloads](https://img.shields.io/github/downloads/strukturart/feedolin/total)
+
+An easy to use calendar for KaiOs
+
+## How to install
+
+The app is not available through the https://www.kaiostech.com/store/ They are only accepting apps that are monetized using <a href="https://developer.kaiostech.com/submit-to-kaistore">KaiAds SDK</a>, and that's something I'm not interested in integrating.
+For this reason, you have to side-load (upload) the app to your phone if you want to use it.
+Martin Kaptein wrote a comprehensive, <a href="https://www.martinkaptein.com/blog/sideloading-and-deploying-apps-to-kai-os/">step-by-step article</a> that you can use to side-load the app. If you prefer a video, this one on YouTube walks you through the process.
+The Developer Portal also contains a guide, and if nothing seems to work, you can <a href="https://discord.com/invite/rQ93zEu">ask for help on Discord</a>.
+You can download the latest version from the Releases page.
+The app is not auto-updating. To update it, you have to follow the same steps you took when installing it.
+
+## How to use
+
+## to do
+
+## Donation
+
+If you use the app often, please donate an amount to me.
+<br>
+
+<table class="border-0"> 
+  <tr class="border-0" >
+    <td valign="top" class="border-0">
+        <div>
+            <a href="https://paypal.me/strukturart?locale.x=de_DE" target="_blank">
+                <img src="/images/paypal.png" width="120px">
+            </a>
+        </div>
+    </td>
+    <td valign="top" class="border-0">
+        <div>
+            <div>Bitcoin</div>
+            <img src="/images/bitcoin_rcv.png" width="120px">
+        </div>
+    </td>
+  </tr>
+ </table>
---
+++ application/index.js
@@ -0,0 +1,426 @@
+"use strict";
+
+let status = {
+  view: "month",
+  selected_day: "",
+  visible: "",
+};
+
+let events;
+
+if (localStorage.getItem("events") != null) {
+  events = JSON.parse(localStorage.getItem("events"));
+} else {
+  events = [];
+}
+
+document.addEventListener("DOMContentLoaded", function () {
+  let today = new Date();
+  let currentMonth = today.getMonth();
+  let currentYear = today.getFullYear();
+  let currentDay = today.getDate();
+  //let selectYear = document.getElementById("year");
+  //let selectMonth = document.getElementById("month");
+  let months = [
+    "Jan",
+    "Feb",
+    "Mar",
+    "Apr",
+    "May",
+    "Jun",
+    "Jul",
+    "Aug",
+    "Sep",
+    "Oct",
+    "Nov",
+    "Dec",
+  ];
+
+  let jump_to_today = function () {
+    let currentMonth = today.getMonth();
+    let currentYear = today.getFullYear();
+    showCalendar(currentMonth, currentYear);
+    document.querySelectorAll(".item")[currentDay - 1].focus();
+    document.querySelectorAll(".item")[currentDay - 1].classList.add("today");
+    status.selected_day = document.activeElement.getAttribute("data-date");
+  };
+
+  function next() {
+    currentYear = currentMonth === 11 ? currentYear + 1 : currentYear;
+    currentMonth = (currentMonth + 1) % 12;
+    showCalendar(currentMonth, currentYear);
+  }
+
+  function previous() {
+    currentYear = currentMonth === 0 ? currentYear - 1 : currentYear;
+    currentMonth = currentMonth === 0 ? 11 : currentMonth - 1;
+    showCalendar(currentMonth, currentYear);
+  }
+
+  function jump() {
+    //currentYear = parseInt(selectYear.value);
+    //currentMonth = parseInt(selectMonth.value);
+    showCalendar(currentMonth, currentYear);
+  }
+
+  let event_check = function (date) {
+    let f;
+
+    for (let t = 0; t < events.length; t++) {
+      f = false;
+
+      if (date === events[t].date) {
+        f = true;
+        t = 1000;
+      }
+    }
+    return f;
+  };
+
+  let monthAndYear = document.getElementById("monthAndYear");
+  showCalendar(currentMonth, currentYear);
+  jump_to_today();
+
+  //////////////
+  //BUILD CALENDAR
+  //////////////
+
+  function showCalendar(month, year) {
+    //let month_year = months[month] + " " + year;
+
+    helper.bottom_bar("view", "select", "options");
+
+    let firstDay = new Date(year, month).getDay();
+    let daysInMonth = 32 - new Date(year, month, 32).getDate();
+
+    let tbl = document.getElementById("calendar-body"); // body of the calendar
+
+    // clearing all previous cells
+    tbl.innerHTML = "";
+
+    // filing data about month and in the page via DOM.
+    monthAndYear.innerHTML = months[month] + " " + year;
+    //selectYear.value = year;
+    //selectMonth.value = month;
+
+    // creating all cells
+    let date = 1;
+    for (let i = 0; i < 6; i++) {
+      // creates a table row
+      let row = document.createElement("div");
+      row.classList.add("flex");
+      row.classList.add("width-100");
+
+      //creating individual cells, filing them up with data.
+      for (let j = 0; j < 7; j++) {
+        if (i === 0 && j < firstDay) {
+          let cell = document.createElement("div");
+          let cellText = document.createTextNode("");
+          cell.appendChild(cellText);
+          row.appendChild(cell);
+        } else if (date > daysInMonth) {
+          break;
+        } else {
+          let cell = document.createElement("div");
+          let cellText = document.createTextNode(date);
+          // color today's date
+          cell.appendChild(cellText);
+          cell.setAttribute("tabindex", date - 1);
+          let p = currentYear + "-" + (currentMonth + 1) + "-" + date;
+          cell.setAttribute("data-date", p);
+          if (event_check(p)) {
+            cell.classList.add("event");
+          }
+
+          cell.classList.add("item");
+          row.appendChild(cell);
+          date++;
+        }
+      }
+
+      tbl.appendChild(row);
+    }
+
+    document.querySelectorAll(".item")[0].focus();
+    status.seleted_day = document.activeElement.getAttribute("data-date");
+    console.log(status.selected_day);
+  }
+
+  /////////////////
+  ///NAVIGATION
+  /////////////////
+
+  let nav = function (move) {
+    console.log(console.log(JSON.stringify(status)));
+    const currentIndex = document.activeElement.tabIndex;
+    const next = currentIndex + move;
+    let items;
+
+    if (status.view == "month") {
+      items = document.querySelectorAll(".item");
+    }
+    if (status.view == "add-edit-event" || status.view == "list-view") {
+      if (
+        document.activeElement.parentNode.classList.contains("input-parent")
+      ) {
+        document.activeElement.parentNode.focus();
+      }
+
+      let b = document.activeElement.parentNode;
+      items = b.querySelectorAll(".item");
+    }
+
+    const targetElement = items[next];
+    targetElement.focus();
+
+    if (status.view == "month") {
+      status.selected_day = targetElement.getAttribute("data-date");
+    }
+    console.log(status.selected_day);
+  };
+
+  ///////////////
+  //STORE EVENTS//
+  ///////////////
+
+  let store_event = function () {
+    let event = {
+      id: today.getTime() / 1000,
+      title: document.getElementById("event-title").value,
+      date: document.getElementById("event-date").value,
+      time: document.getElementById("event-time").value,
+      description: document.getElementById("event-description").value,
+    };
+
+    events.push(event);
+    localStorage.setItem("events", JSON.stringify(events));
+    status.view = "month";
+    router();
+  };
+
+  let set_tabindex = function () {
+    document.querySelectorAll("article").forEach(function (i, p) {
+      i.setAttribute("tabindex", p);
+    });
+  };
+
+  ////////////////////
+  ////BUILD EVENT-LIST
+  ///////////////////
+
+  function renderHello(arr) {
+    var template = document.getElementById("template").innerHTML;
+    var rendered = Mustache.render(template, {
+      data: arr,
+    });
+    document.getElementById("list-view").innerHTML = rendered;
+    set_tabindex();
+  }
+
+  renderHello(events);
+
+  ///////////////////
+  ///ROUTER
+  /////////////////
+
+  const month = document.getElementById("calendar");
+  const add_edit_event = document.getElementById("add-edit-event");
+  const list_view = document.getElementById("list-view");
+
+  const pages = document.querySelectorAll(".page");
+
+  let router = function (view) {
+    pages.forEach(function (index) {
+      index.style.display = "none";
+    });
+
+    if (view == "view") {
+      if (status.view == "month") {
+        status.view = "list-view";
+      } else {
+        status.view = "month";
+      }
+    }
+
+    if (status.view == "add-edit-event") {
+      document.getElementById(
+        "event-date"
+      ).value = document.activeElement.getAttribute("data-date");
+      add_edit_event.style.display = "block";
+      add_edit_event.querySelectorAll(".item")[0].focus();
+      helper.bottom_bar("", "edit", "");
+      return true;
+    }
+
+    if (status.view == "month") {
+      month.style.display = "block";
+    }
+    if (status.view == "list-view") {
+      console.log(status.selected_day);
+
+      list_view.style.display = "block";
+      renderHello(events);
+
+      document.querySelectorAll("[data-fdate]").forEach(function (e) {
+        if (e.getAttribute("data-fdate") == status.selected_day) {
+          e.focus();
+        }
+      });
+    }
+  };
+
+  //////////////////////////////
+  ////KEYPAD HANDLER////////////
+  //////////////////////////////
+
+  let longpress = false;
+  const longpress_timespan = 1000;
+  let timeout;
+
+  function repeat_action(param) {
+    switch (param.key) {
+      case "0":
+        break;
+    }
+  }
+
+  //////////////
+  ////LONGPRESS
+  /////////////
+
+  function longpress_action(param) {
+    switch (param.key) {
+      case "0":
+        break;
+    }
+  }
+
+  ///////////////
+  ////SHORTPRESS
+  //////////////
+
+  function shortpress_action(param) {
+    switch (param.key) {
+      case "*":
+        jump_to_today();
+
+        break;
+
+      case "ArrowUp":
+        if (status.view == "month") {
+          nav(-7);
+        }
+        if (status.view == "add-edit-event" || status.view == "list-view") {
+          nav(-1);
+        }
+        break;
+      case "ArrowDown":
+        if (status.view == "month") {
+          nav(+7);
+        }
+        if (status.view == "add-edit-event" || status.view == "list-view") {
+          nav(+1);
+        }
+
+        break;
+      case "ArrowRight":
+        nav(1);
+        break;
+      case "ArrowLeft":
+        nav(-1);
+        break;
+
+      case "1":
+        previous();
+        break;
+      case "3":
+        next();
+        break;
+
+      case "SoftLeft":
+      case "Control":
+        router("view");
+        break;
+
+      case "Enter":
+        if (document.activeElement.classList.contains("input-parent")) {
+          document.activeElement.children[1].focus();
+          return true;
+        }
+        if (status.view == "month") {
+          status.view = "add-edit-event";
+          router();
+        }
+
+        if (document.activeElement.id == "save-event") {
+          store_event();
+        }
+        break;
+
+      case "Backspace":
+        if (status.view == "add-edit-event") {
+          status.view = "month";
+          router();
+        }
+
+        break;
+    }
+  }
+
+  /////////////////////////////////
+  ////shortpress / longpress logic
+  ////////////////////////////////
+
+  function handleKeyDown(evt) {
+    if (evt.key === "Backspace") {
+      evt.preventDefault();
+    }
+
+    if (evt.key === "EndCall") {
+      evt.preventDefault();
+      helper.goodbye();
+    }
+    if (!evt.repeat) {
+      longpress = false;
+      timeout = setTimeout(() => {
+        longpress = true;
+        longpress_action(evt);
+      }, longpress_timespan);
+    }
+
+    if (evt.repeat) {
+      if (evt.key == "Backspace") evt.preventDefault(); // Disable close app by holding backspace
+
+      longpress = false;
+      repeat_action(evt);
+    }
+  }
+
+  function handleKeyUp(evt) {
+    evt.preventDefault();
+
+    if (evt.key == "Backspace") evt.preventDefault(); // Disable close app by holding backspace
+
+    if (
+      evt.key == "Backspace" &&
+      status.window_status != "article-list" &&
+      document.activeElement.tagName == "INPUT"
+    ) {
+      evt.preventDefault();
+    }
+
+    clearTimeout(timeout);
+    if (!longpress) {
+      shortpress_action(evt);
+    }
+  }
+
+  document.addEventListener("keydown", handleKeyDown);
+  document.addEventListener("keyup", handleKeyUp);
+
+  document.addEventListener("visibilitychange", function () {
+    setTimeout(function () {
+      status.visible = document.visibilityState;
+    }, 1000);
+  });
+});
