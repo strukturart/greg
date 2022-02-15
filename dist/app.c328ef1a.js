566ff6e446799eeb1e72b8ec542280b4bb93af37
strukturart <print@strukturart.com>
2022-02-15 23:27:23 +0100

    Auto stash before merge of "master" and "newjs-style"

--- dist/app.c328ef1a.js
+++ dist/app.c328ef1a.js
@@ -29475,7 +29475,7 @@ var parent = module.bundle.parent;
 if ((!parent || !parent.isParcelRequire) && typeof WebSocket !== 'undefined') {
   var hostname = "" || location.hostname;
   var protocol = location.protocol === 'https:' ? 'wss' : 'ws';
-  var ws = new WebSocket(protocol + '://' + hostname + ':' + "46327" + '/');
+  var ws = new WebSocket(protocol + '://' + hostname + ':' + "42279" + '/');
 
   ws.onmessage = function (event) {
     checkedAssets = {};
